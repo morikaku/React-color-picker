343c35b47b9726e6f45c4b13fa9fac87b2c26420
morikaku <y.morikaku@outlook.com>
2024-04-03 06:48:49 +0900

    layout adjust

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,21 +1,42 @@
 import React from "react";
 import { SketchPicker } from "react-color";
+import style from "./ColorPicker.module.css";
 
 class ColorPicker extends React.Component {
-    state = {
-        color: "#fff",
-    };
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            color: "#9999ff",
+        };
+    }
+
     handleChange = (color) => {
         this.setState({ color: color.hex });
     };
-    
+
+    handleOpen = () => {
+        this.setState({ open: !this.state.open });
+    }
+
+    handleClose = () => {
+        this.setState({ open: false });
+    }
+
     render() {
         return (
-            <div>
-            <SketchPicker color={this.state.color} onChange={this.handleChange} />
-            </div>
-            );
-        }
+            <>
+                <div className={style.container} style={{ background: this.state.color }}>
+                    <h1 className={style.color__title}>Color Picker</h1>
+                    <div className={style.color__text}>Selected : {this.state.color}</div>
+                    <button className={style.color__button} onClick={this.handleOpen}>Select color</button>
+                    {this.state.open && (
+                        <div className={style.color__overlay} onClick={this.handleClose}></div>
+                    )}
+                    <SketchPicker color={this.state.color} onChange={this.handleChange} />
+                </div>
+            </>
+        );
     }
+}
 export default ColorPicker;
